07f031b32b8e7516b4bfeb880e72b33509ad30ba
Eric Gourlaouen <e.gourlaouen@weezevent.com>
2016-03-01 14:06:45 +0100

    added id to article

--- perm/serializers.py
+++ perm/serializers.py
@@ -12,11 +12,12 @@ class PermSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     ventes = serializers.IntegerField(read_only=True)
     ventes_last_update = serializers.DateTimeField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = perm_models.Article
         fields = ('stock', 'nom', 'prix', 'perm', 'ventes', 'tva',
-                  'ventes_last_update', 'id_payutc')
+                  'ventes_last_update', 'id_payutc', 'id')
 
 ArticleListSerializer = ArticleSerializer.many_init
 
