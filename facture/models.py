a866e3c1416da7ecb835ae07ba55e339f7740135
Eric Gourlaouen <e.gourlaouen@weezevent.com>
2016-03-13 23:39:22 +0100

    Changed relationship between facture and cheque

--- facture/models.py
+++ facture/models.py
@@ -40,7 +40,7 @@ class Cheque(models.Model):
     state = models.CharField(max_length=1, choices=CHEQUE_STATES)
     destinataire = models.CharField(max_length=255, null=True, default=None)
     commentaire = models.TextField(null=True, default=None)
-    facturerecue = models.ForeignKey(FactureRecue, null=True)
+    facturerecue = models.OneToOneField(FactureRecue, null=True)
 
 
 class FactureEmise(models.Model):
