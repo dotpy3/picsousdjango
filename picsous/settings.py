cf39bc988345350d43dd4ac6ec03141bb19263d0
Eric Gourlaouen <gourlaoueneric@gmail.com>
2017-01-17 08:53:21 +0100

    added travis file

--- picsous/settings.py
+++ picsous/settings.py
@@ -187,3 +187,16 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
