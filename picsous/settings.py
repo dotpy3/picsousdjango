b9f13eec31ec802126f32307307d42f02b7d81ef
Eric Gourlaouen <gourlaoueneric@gmail.com>
2017-01-17 09:56:28 +0100

    finally found source of dal

--- picsous/settings.py
+++ picsous/settings.py
@@ -24,8 +24,6 @@ DEBUG = False
 # Application definition
 
 INSTALLED_APPS = [
-    'dal',
-    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
